5471ea4d34f259e634d01338fa26123ff8efbe27
Protsvetov Danila <dprotsvetov@gmail.com>
2017-02-06 20:33:56 +0400

    commit

---
+++ src/numbers/app01/Main.java
@@ -0,0 +1,14 @@
+/*
+ * @(#)Main.java 1.0 06.02.2017
+ */
+package numbers.app01;
+
+/**
+ * @author Protsvetov Danila
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main()");
+    }
+}
